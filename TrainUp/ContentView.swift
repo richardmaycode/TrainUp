d7ab0ffda9071f221c0eb99bdf2be4351c1f12f1
Richard Wise <richard.wise@hey.com>
2023-07-08 10:44:00 -0400

    Initial Commit

---
+++ TrainUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainUp
+//
+//  Created by Richard Wise on 7/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainUp/TrainUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainUpApp.swift
+//  TrainUp
+//
+//  Created by Richard Wise on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TrainUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
